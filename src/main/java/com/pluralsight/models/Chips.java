411b70ef95d66014097f36ec25d8fd1e18be43cf
Eden Mengistu <eden.g.mengistu@gmail.com>
2025-05-28 10:36:21 -0500

    draft

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,4 +1,17 @@
 package com.pluralsight.models;
 
-public class Chips {
+import com.pluralsight.ui.OrderScreen;
+
+public class Chips extends OrderScreen {
+    private String name
+    private double price = 1.50;
+    public Chips(String name) {
+        this.name = name;
+    }
+     @Override
+    public String getDescription() {return name + "Chips";}
+    @Override
+    public double getPrice(){ return price;
+
+    }
 }
--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,4 +1,24 @@
 package com.pluralsight.models;
 
-public class Drink {
+import com.pluralsight.ui.OrderScreen;
+
+public class Drink implements OrderScreen {
+    private String name;
+    private String size;
+    private double price;
+
+    public Drink(String name, String size) {
+        this.name = name;
+        this.size = size;
+        switch (size){
+            case "Small":price =2.00;break;
+            case "Medium": price= 2.50;break;
+            case "Large" : price=3.00;break;
+
+        }
+    }
+    @Override
+    public String getDescription(){return size + " " +name
+
+    }
 }
